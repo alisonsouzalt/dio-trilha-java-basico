e42223111c377aaf9b260b52cc4f6daee2862fd9
Alison Souza <alisonsouzalt@hotmail.com>
2024-10-09 20:44:33 -0300

    att

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,14 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        double salarioMinimo = 2500.33;
+
+        int numero = 5;
+        numero = 10;
+
+        System.out.println(numero);
+
+        final double VALOR_DE_PI = 10.75;
+
+
     }
 }
