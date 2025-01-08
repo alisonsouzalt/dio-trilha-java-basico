e89d37a5d8e63362a73c2f69eb2c8f7a1e433f22
alisonsouzalt <you@example.com>
2025-01-08 17:23:45 -0300

    Condição encadeada

--- controle-fluxo/src/ResultadoEscolar.java
+++ controle-fluxo/src/ResultadoEscolar.java
@@ -1,11 +1,18 @@
 public class ResultadoEscolar {
     public static void main(String[] args) {
-        int nota = 7;
+        int nota = 6;
 
-        if(nota >=7)
+        if(nota >=7){
         System.out.println("Aprovado");
+        }
 
-        else
+        else if(nota >= 5 && nota < 7){
+        System.out.println("Prova de Recuperação");
+
+        }
+
+        else{
         System.out.println("Reprovado");
+        }
     }
 }
