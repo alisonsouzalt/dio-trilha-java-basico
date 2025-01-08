35ac71f4f1bc29b5e59b7c5bd16d1577516e2c1b
alisonsouzalt <you@example.com>
2025-01-08 17:35:53 -0300

    up

--- controle-fluxo/src/ResultadoEscolar.java
+++ controle-fluxo/src/ResultadoEscolar.java
@@ -2,6 +2,9 @@ public class ResultadoEscolar {
     public static void main(String[] args) {
         int nota = 6;
 
+        String resultado = nota >= 7? "Aprovado" : nota >=5 && nota <=7? "Recuperação" : "Reprovado";
+        System.out.println(resultado);
+
         if(nota >=7){
         System.out.println("Aprovado");
         }
