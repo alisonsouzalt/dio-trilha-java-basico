9ea5fd61071e8e074bc92ba83fccd78c708dae2c
alisonsouzalt <you@example.com>
2025-02-03 23:38:17 -0300

    sistema bancario

---
+++ desafio-dio-banco/src/Apresentacoes.java
@@ -0,0 +1,35 @@
+public class Apresentacoes{
+
+    public void menu(){
+        System.out.println("Escolha uma opção abaixo:");
+        System.out.println("1. Criar conta");
+    }
+    public void menuUm(){
+        System.out.println("Escolha uma opção abaixo:");
+        System.out.println("1. Deposito");
+        System.out.println("2. Saque");
+        System.out.println("3. Transferencia");
+        System.out.println("4. Saldo");
+        System.out.println("0. Sair do sistema");
+    }
+    public void contaCriada(){
+        System.out.println("Conta criada com sucesso!");
+        System.out.println("########################\n");
+    }
+    public void depositoContas(){
+        System.out.println("Escolha a conta para deposito:");
+        System.out.println("1. Corrente");
+        System.out.println("2. Poupanca");
+    }
+    public void transferenciaC(){
+        System.out.println("Escolha a conta para transferencia:");
+        System.out.println("1. Corrente");
+        System.out.println("2. Poupanca");
+    }
+    public void saqueC(){
+        System.out.println("Escolha a conta para saque:");
+        System.out.println("1. Corrente");
+        System.out.println("2. Poupanca");
+    }
+
+}
---
+++ desafio-dio-banco/src/Main.java
@@ -0,0 +1,114 @@
+import java.util.Scanner;
+import java.util.Locale;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        Cliente cliente = new Cliente();
+        Apresentacoes ap = new Apresentacoes();
+        int escolha = 0;
+        String texto;
+        double valor = 0;
+
+        System.out.println("## SEJA BEM VINDO!\n");
+        ap.menu();
+        escolha = scanner.nextInt();
+        if (escolha == 1){
+            System.out.println("Digite seu nome");
+            texto = scanner.next();
+            cliente.setNome(texto);
+            ContaCorrente cc = new ContaCorrente(cliente);
+            ContaPoupanca cp = new ContaPoupanca(cliente);
+            ap.contaCriada();
+
+            while (true){
+                ap.menuUm();
+                escolha = scanner.nextInt();
+                if (escolha == 1) {
+                    ap.depositoContas();
+                    escolha = scanner.nextInt();
+                    if (escolha == 1) {
+                        System.out.println("Digite o valor de deposito:");
+                        valor = scanner.nextDouble();
+                        if (valor > 0) {
+                            cc.depositar(valor);
+                            System.out.println("Deposito de R$ " + valor + " realizado com sucesso\n");
+                        } else {
+                            System.out.println("O valor deve ser maior que 0\n");
+                        }
+                    } else if (escolha == 2) {
+                        System.out.println("Digite o valor de deposito:");
+                        valor = scanner.nextDouble();
+                        if (valor > 0) {
+                            cp.depositar(valor);
+                            System.out.println("Deposito de R$ " + valor + " realizado com sucesso\n");
+                        } else {
+                            System.out.println("O valor deve ser maior que 0\n");
+                        }
+                    }
+                }
+                else if(escolha == 2){
+                    ap.saqueC();
+                    escolha = scanner.nextInt();
+                    if (escolha == 1){
+                        System.out.println("Digite o valor de saque:");
+                        valor = scanner.nextDouble();
+                        if (valor <= cc.saldo) {
+                            cc.sacar(valor);
+                            System.out.println("Saque de R$ " + valor + " realizado com sucesso\n");
+                        }
+                        else{
+                            System.out.println("Saldo insuficiente!\n");
+                        }
+                    }
+                    else if (escolha == 2){
+                        System.out.println("Digite o valor de saque:");
+                        valor = scanner.nextDouble();
+                        if (valor <= cp.saldo) {
+                            cp.sacar(valor);
+                            System.out.println("Saque de R$ " + valor + " realizado com sucesso\n");
+                        }
+                        else{
+                            System.out.println("Saldo insuficiente!\n");
+                        }
+                    }
+                }
+                else if (escolha == 3) {
+                    ap.transferenciaC();
+                    escolha = scanner.nextInt();
+                    if (escolha == 1) {
+                        System.out.println("Digite o valor de transferencia:");
+                        valor = scanner.nextDouble();
+                        if (valor < cp.saldo){
+                            cp.transferir(valor, cc);
+                            System.out.println("Transferencia de R$ " + valor + " realizada com sucesso\n");
+                        }
+                        else{
+                            System.out.println("Saldo insuficiente!\n");
+                        }
+                    }
+                    else if (escolha == 2) {
+                        System.out.println("Digite o valor de transferencia:");
+                        valor = scanner.nextDouble();
+                        if (valor < cc.saldo) {
+                            cc.transferir(valor, cp);
+                            System.out.println("Transferencia de R$ " + valor + " realizada com sucesso\n");
+                        }
+                        else{
+                            System.out.println("Saldo insuficiente!\n");
+                        }
+                    }
+                }
+                else if (escolha == 4){
+                    cc.imprimirExtrato();
+                    cp.imprimirExtrato();
+                }
+                else if (escolha == 0){
+                    System.out.println("Sistema finalizado!");
+                    break;
+                }
+
+            }
+        }
+
+    }
+}
