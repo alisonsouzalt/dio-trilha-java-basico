77f48866015047aa09ddd492997f78f0f9140bfd
alisonsouzalt <you@example.com>
2025-02-20 18:21:45 -0300

    up

---
+++ praticando/src/estruturaDados/youtube/Programa.java
@@ -0,0 +1,10 @@
+package estruturaDados.youtube;
+
+public class Programa {
+    public static void main(String[] args) {
+            ListaEncadeada<Integer> lista = new ListaEncadeada<>();
+            lista.adiciona(1);
+
+            System.out.println(lista);
+    }
+}
